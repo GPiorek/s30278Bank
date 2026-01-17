878b1ec4b4fd9bc986de58168c891ab6e8b17620
GPiorek <GrzegorzPiorekkk@outlook.com>
2026-01-17 10:23:06 +0100

    Initial commit

---
+++ src/main/java/pio/grz/s30278bank/S30278BankApplication.java
@@ -0,0 +1,13 @@
+package pio.grz.s30278bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S30278BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S30278BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pio/grz/s30278bank/S30278BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pio.grz.s30278bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S30278BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
